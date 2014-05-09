dd7e64b0bc60981d882904cbcaf7315c45345e2a
Ryan Grow <ryan.grow@canoeventures.com>
2014-05-09 11:30:58 -0600

    use h2

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,33 +17,14 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "none"
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-            dbCreate = "none"
-            url = "jdbc:mysql://grailsvm/angugrails?useUnicode=yes&characterEncoding=UTF-8"
-            username = "angugrails"
-            password = "angugrails"
-        }
-    }
-    development_h2 {
-        dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     test {
-        dataSource_h2 {
+        dataSource {
             dbCreate = "create-drop"
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-        dataSource {
-            dbCreate = "none"
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-            url = "jdbc:mysql://grailsvm/angugrails_test?useUnicode=yes&characterEncoding=UTF-8"
-            username = "angugrails_test"
-            password = "angugrails_test"
         }
     }
 }
